9348e15fddd1f42c6c38646651329c9a14c97912
mzfirozuddin <mz.firozuddin@gmail.com>
2023-11-12 23:28:59 +0530

    utils code added

--- src/index.js
+++ src/index.js
@@ -2,14 +2,23 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MongoDB connection error !!!", err);
+})
 
 
 
 
-//- This is a way to connect database 
+//- This is another way to connect database 
 /* 
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants";
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,31 @@
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+export { asyncHandler };
+
+
+
+
+
+//- higher order func concept
+// const asyncHandler = () => {}
+// const asyncHandler = (fn) => { () => {} }
+// const asyncHandler = (fn) =>  async () => {} 
+
+
+//- asyncHandler function using try-catch
+/* 
+const asyncHandler = (fn) => async (req, res, next) => {
+    try {
+        await fn(req, res, next)
+    } catch (error) {
+        res.status(error.code || 500).json({
+            success: false,
+            message: error.message
+        })
+    }
+} 
+*/
